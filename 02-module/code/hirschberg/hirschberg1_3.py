071d791b0d085ec9362e209f91525649adee233f
Sean P. Goggins <s@goggins.com>
2022-02-13 17:06:00 -0600

    more print statements.

--- 02-module/code/hirschberg/hirschberg1_3.py
+++ 02-module/code/hirschberg/hirschberg1_3.py
@@ -4,15 +4,18 @@ from functions1_3 import backwards
 from functions1_3 import hirschberg
 
 # Read alphabet and scores from text file
+print('Hirschberg: Reading alphabet scores from the text files scores.txt')
 f = open("scores.txt", 'r')
 alphabet = f.readline()
 while(alphabet[-1] in ['\n', '\r']):
     alphabet = alphabet[:-1]
 f.readline()
 gapPenalty = int(f.readline())
+print(f'Hirschberg: This lines gap penalty is: {gapPenalty}')
 f.readline()
 simMatrix = []
 line = f.readline()
+print('Hirschberg: Building a matrix.')
 while(line):
     row = list(int(x) for x in line.split())
     simMatrix.append(row)
@@ -20,15 +23,20 @@ while(line):
 f.close()
 
 # Create a 1-1 mapping from characters to integers, for simplicity in algorithm
+print(f'Hirschberg: Creating a 1-1 mapping from characters to integers, for simplicity in algorithm')
 alphEnum = dict([(alphabet[i], i) for i in range(len(alphabet))])
 
 # Load input sequences
+print('Hirschberg: Loading input sequences.')
 f = open("sequences.txt", 'r')
 line = f.readline()
 
 # Open output file, in preparation for storing output alignments
+print('Opening output file alignments.txt in preparation for storing output alignments.')
 g = open("alignments.txt", 'w')
 
+
+print('OK. Hirschberg: This loop repeats until no more input sequences are found At each iteration we read the next two sequences and run the algorithm on them')
 while(line):
     # This loop repeats until no more input sequences are found
     # At each iteration we read the next two sequences and run the algorithm on them
