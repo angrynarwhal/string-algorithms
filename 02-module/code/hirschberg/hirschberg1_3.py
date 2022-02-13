d9cfcd3b4128e6dea5918f76859ee4a7783cc568
Sean P. Goggins <s@goggins.com>
2022-02-13 07:49:35 -0600

    hirschberg converted to python3

--- 02-module/code/hirschberg/hirschberg1_3.py
+++ 02-module/code/hirschberg/hirschberg1_3.py
@@ -43,15 +43,15 @@ while(line):
 
     # Run the Hirschberg algorithm
 
-    print "First sequence:", A
-    print "Second sequence:", B
-    print "Calculating alignment distance by Hirschberg method..."
+    print(f"First sequence: {A}")
+    print(f"Second sequence: {B}")
+    print("Calculating alignment distance by Hirschberg method...")
     
     z = hirschberg(A, B, simMatrix, gapPenalty, alphEnum)
 
-    print "Alignment of A: ", z[0]
-    print "Alignment of B: ", z[1]
-    print "Similarity score: ", z[2], '\n'
+    print(f"Alignment of A: {z[0]} \n ")  
+    print(f"Alignment of B: {z[1]} \n ")
+    print(f"Similarity score: {z[2]} \n ")
 
     # Write outputs to text file
     g.write(str(z[2]) + "\n")
